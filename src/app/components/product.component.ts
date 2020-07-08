b19f584d892e0d4c2b803eba804b389b513b24a9
droogadulce <ahigareda.mayra@ciencias.unam.mx>
2020-07-08 17:55:16 -0500

    Component life cycle

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges,
+    OnInit,
+    DoCheck,
+    OnDestroy
+} from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -6,10 +16,59 @@ import { Product } from '../product.model';
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    /**
+     * First to run.
+     * Best place to inyect component dependencies.
+     */
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    /**
+     * This event is executed every time a value of an input control is changed
+     * within a component. It is activated first when the value of a linked
+     * property is changed. You always receive a change data map,
+     * which contains the current and previous value of the linked property wrapped
+     * in a SimpleChange.
+     */
+    // ngOnChanges(changes: SimpleChanges): void {
+    //     console.log('2. ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    /**
+     * It runs once Angular has deployed the data-bound properties or when the
+     * component has been initialized, once ngOnChanges has been run. This event
+     * is mainly used to initialize the data in the component.
+     * Best place to call data services.
+     */
+    ngOnInit(): void {
+        console.log('3. ngOnInit');
+    }
+
+    /**
+     * It is activated every time the input properties of a component are verified.
+     * This method allows us to implement our own custom change detection algorithm
+     * or logic for any component.
+     * Can't be implemented at the same time as ngOnChanges.
+     */
+    ngDoCheck(): void {
+        console.log('4. ngDoCheck');
+    }
+
+    /**
+     * This method will be executed just before Angular destroys the components.
+     * It is very useful to unsubscribe from observables and disconnect event
+     * handlers to avoid memory leaks.
+     */
+    ngOnDestroy(): void {
+        console.log('5. ngOnDestroy');
+    }
 
     addToCart(): void {
         console.log('product added');
